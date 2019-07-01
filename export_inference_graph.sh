c9e311c87036aa4d38d0e9ff2b8c640101652c70
lianchunliu <lianchun.liu@gmail.com>
2019-07-01 23:15:28 +0800

    update

--- export_inference_graph.sh
+++ export_inference_graph.sh
@@ -4,5 +4,5 @@
 python object_detection/export_inference_graph.py \
     --input_type image_tensor \
     --pipeline_config_path /floyd/home/family-detection/ssd_mobilenet_v2_coco.config \
-    --trained_checkpoint_prefix /floyd/home/family_detection_train/model.ckpt-4031 \
+    --trained_checkpoint_prefix /floyd/input/family_model/model.ckpt \
     --output_directory /floyd/home/family_export_inference_graph
