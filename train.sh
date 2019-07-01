7f6c63ab79aa1dc9a85d1d849e00be9ac3a7ddce
lianchunliu <lianchun.liu@gmail.com>
2019-07-01 08:03:36 +0800

    nothing

--- train.sh
+++ train.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-#git clone https://github.com/floydhub/object-detection-template family-detection
+rm -rf family-detection
 
-#cd  family-detection
+git clone https://github.com/lianchunliu/family-detection.git family-detection
+
+cd  family-detection
 
 export PYTHONPATH=".:/floyd/home/family-detection:/floyd/home/family-detection/slim:$PYTHONPATH"
 python object_detection/legacy/train.py --train_dir=/floyd/home/family_detection_train/ --pipeline_config_path=/floyd/home/family-detection/ssd_mobilenet_v2_coco.config
