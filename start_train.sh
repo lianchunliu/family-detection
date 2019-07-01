ee7a82fb4ff1053dd9aeca47b4ce749dccb34bde
lianchunliu <lianchun.liu@gmail.com>
2019-07-01 13:10:55 +0800

    update

--- start_train.sh
+++ start_train.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-floyd run --gpu --env tensorflow-1.13 --data lianchun227/datasets/family_data/3:family_data 'bash train.sh'
+floyd run --gpu --env tensorflow-1.13 --data lianchun227/datasets/family_data/3:family_data --data lianchun227/datasets/family_model/1:family_model 'bash train.sh'
 
