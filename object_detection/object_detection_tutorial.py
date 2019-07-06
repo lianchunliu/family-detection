70f47a698626a9a0a5bf4b17483275d81539e006
lianchunliu <lianchun.liu@gmail.com>
2019-07-06 11:03:40 +0800

    upload

--- object_detection/object_detection_tutorial.py
+++ object_detection/object_detection_tutorial.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Object Detection Demo
-# Welcome to the object detection inference walkthrough!  This notebook will walk you step by step through the process of using a pre-trained model to detect objects in an image. Make sure to follow the [installation instructions](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/installation.md) before you start.
-
-# # Imports
-
-# In[1]:
-
-
 import numpy as np
 import os
 import six.moves.urllib as urllib
@@ -30,67 +22,15 @@ from object_detection.utils import ops as utils_ops
 if StrictVersion(tf.__version__) < StrictVersion('1.12.0'):
   raise ImportError('Please upgrade your TensorFlow installation to v1.12.*.')
 
-
-# ## Env setup
-
-# In[2]:
-
-
-# This is needed to display the images.
-#%matplotlib inline
-
-
-# ## Object detection imports
-# Here are the imports from the object detection module.
-
-# In[3]:
-
-
 from utils import label_map_util
 
 from utils import visualization_utils as vis_util
-
-
-# # Model preparation 
-
-# ## Variables
-# 
-# Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_FROZEN_GRAPH` to point to a new .pb file.  
-# 
-# By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
-
-# In[4]:
-
-
-# What model to download.
-#MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
-#MODEL_FILE = MODEL_NAME + '.tar.gz'
-#DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_FROZEN_GRAPH = '/floyd/home/family_export_inference_graph/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = os.path.join('/floyd/home/family-detection/family_label_map.pbtxt')
-
-
-# ## Download Model
-
-# In[5]:
-
-
-#opener = urllib.request.URLopener()
-#opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-#tar_file = tarfile.open(MODEL_FILE)
-#for file in tar_file.getmembers():
- # file_name = os.path.basename(file.name)
- # if 'frozen_inference_graph.pb' in file_name:
- #   tar_file.extract(file, os.getcwd())
-
-
-# ## Load a (frozen) Tensorflow model into memory.
-
-# In[6]:
 
 
 detection_graph = tf.Graph()
@@ -121,24 +61,6 @@ def load_image_into_numpy_array(image):
   return np.array(image.getdata()).reshape(
       (im_height, im_width, 3)).astype(np.uint8)
 
-
-# # Detection
-
-# In[ ]:
-
-
-# For the sake of simplicity we will use only 2 images:
-# image1.jpg
-# image2.jpg
-# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-#PATH_TO_TEST_IMAGES_DIR = 'test_images'
-#TEST_IMAGE_PATHS = [ '/floyd/input/family/img_20190624_232319.jpg']
-
-# Size, in inches, of the output images.
-#IMAGE_SIZE = (12, 8)
-
-
-# In[ ]:
 
 
 def run_inference_for_single_image(image, graph):
@@ -186,9 +108,6 @@ def run_inference_for_single_image(image, graph):
       if 'detection_masks' in output_dict:
         output_dict['detection_masks'] = output_dict['detection_masks'][0]
   return output_dict
-
-
-# In[ ]:
 
 
 #get_ipython().run_line_magic('matplotlib', 'inline')
